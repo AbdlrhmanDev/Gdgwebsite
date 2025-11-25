7361d2aecc9f284dfcf3cbb4b64b00f486276895
Abdullrazaq naqsho <apoodi.805@gmail.com>
2025-11-26 02:15:42 +0300

    4

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,12 +1,32 @@
 const nodemailer = require('nodemailer');
 
 const sendEmail = async (options) => {
-    text: options.message
-};
+    console.log(`Attempting to send email... Host: ${process.env.SMTP_HOST}, Port: ${process.env.SMTP_PORT}, Secure: ${process.env.SMTP_PORT == 465}`);
 
-const info = await transporter.sendMail(message);
+    const transporter = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        secure: process.env.SMTP_PORT == 465, // true for 465, false for other ports
+        auth: {
+            user: process.env.SMTP_EMAIL,
+            pass: process.env.SMTP_PASSWORD
+        },
+        tls: {
+            ciphers: 'SSLv3'
+        },
+        family: 4 // Force IPv4 to avoid IPv6 connection issues on Render
+    });
 
-console.log('Message sent: %s', info.messageId);
+    const message = {
+        from: `${process.env.FROM_NAME} <${process.env.FROM_EMAIL}>`,
+        to: options.email,
+        subject: options.subject,
+        text: options.message
+    };
+
+    const info = await transporter.sendMail(message);
+
+    console.log('Message sent: %s', info.messageId);
 };
 
 module.exports = sendEmail;
